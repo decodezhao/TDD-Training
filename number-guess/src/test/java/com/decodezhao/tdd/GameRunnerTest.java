acbef645f1334ce2afdd19c1f7ea3cea535fed76
decodezhao <decodezhao@gmail.com>
2017-12-13 23:54:24 +0800

    正常場景

--- number-guess/src/test/java/com/decodezhao/tdd/GameRunnerTest.java
+++ number-guess/src/test/java/com/decodezhao/tdd/GameRunnerTest.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GameRunnerTest {
@@ -17,9 +18,12 @@ public class GameRunnerTest {
     @Mock
     private ScannerHandler scannerHandler;
 
+    @Mock
+    private NumberGuess numberGuess;
+
     @Before
     public void setUp() {
-        gameRunner = new GameRunner(scannerHandler);
+        gameRunner = new GameRunner(scannerHandler, numberGuess);
     }
 
 
@@ -28,6 +32,17 @@ public class GameRunnerTest {
         when(scannerHandler.readLine()).thenReturn("");
         String result = gameRunner.start();
         Assert.assertEquals("quit", result);
+    }
+
+    @Test
+    public void shouldReturn4A0BWhenReadLine_1234() {
+        when(numberGuess.guess(any(UniqueNumber.class))).thenReturn("4A0B");
+        when(scannerHandler.readLine()).thenReturn("1234");
+
+        gameRunner.start();
+
+        verify(numberGuess, times(1)).guess(new UniqueNumber("1234"));
+
     }
 
 }
